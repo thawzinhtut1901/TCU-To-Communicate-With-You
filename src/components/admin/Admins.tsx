38823c9c6a21d269b6d3b704c0b79b87262772be
thawzinhtut <thawzinhtut9421@gmail.com>
2024-09-05 21:38:10 -0700

    Admins UI & Admin Profile UI, API

--- src/components/admin/Admins.tsx
+++ src/components/admin/Admins.tsx
@@ -1,7 +1,128 @@
+import { friSuggestionProfile, Fliter } from "@/assets"
+import { AiOutlineDown } from "react-icons/ai"
+import { BiSolidCircle } from "react-icons/bi"
+import { IoIosSearch } from "react-icons/io"
+import { Button } from "../ui/button"
+import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs"
+import { Pagination, Stack } from "@mui/material";
+import { BsPlusLg } from "react-icons/bs";
 
 const Admins = () => {
   return (
-    <div>Admins</div>
+    <div className="overflow-auto vertical-scrollbar">
+      <div className="flex flex-col mx-[40px]">
+        <h1 className="my-[20px] font-bold font-roboto text-[28px]">Admins</h1>
+
+        <div className="flex">
+          <div className="bg-white shadow-md mr-2 p-2 rounded-[10px]">
+            <img src={Fliter} alt="" className="w-[18px] h-[18px]"/>
+          </div>
+
+          <div className="relative">
+              <IoIosSearch
+                className="top-[7px] right-2 absolute"
+                size="20px"
+                color="gray"
+              />
+              <input
+                className="shadow-md px-4 py-1 rounded-[6px] w-[320px] h-[34px]"
+                placeholder="Search..."
+              />
+          </div>
+
+          <div className="flex gap-x-1 bg-[#591DA9] ml-auto p-2 rounded-[8px] text-[14px] text-white">
+            <BsPlusLg className="mt-[2px] w-[15px] h-[15px]"/>
+            <h1>Add Admin</h1>
+          </div>
+        </div>
+
+        <div className="bg-white shadow-custom-grey-inner mt-[24px] rounded-[8px] cursor-pointer">
+          <ul  className="gap-x-5 grid grid-cols-7 opacity-85 py-[15px] border-b border-b-slate-400 font-primary text-[16px] text-center uppercase">
+            <li className="mx-auto"></li>
+            <li>admin name</li>
+            <li>role</li>
+            <li>status</li>
+            <li>kpi</li>
+            <li>employed</li>
+            <li>action</li>
+          </ul>
+
+              <ul  className="items-center gap-x-5 grid grid-cols-7 py-[15px] border-b border-b-slate-400 font-roboto text-[14px] text-center"> 
+                <li className="mx-auto">
+                  <img src={friSuggestionProfile} alt="" className="rounded-[8px] w-[40px] h-[40px]"/>
+                </li>
+                <li className="flex flex-col text-slate-700 text-start">
+                  <h1>Thaw Gyi</h1>
+                  <p className="text-[12px] text-slate-500">thaw@gmail.com</p>
+                </li>
+                <li className="text-slate-500">Manager</li>
+                <li className="flex justify-center gap-x-1">
+                  <BiSolidCircle className="border-[#52825F] mt-[6.2px] border rounded-full w-[8px] h-[8px] text-[#34A853]"/>
+                  <span className="text-[#34A853]">Active</span>
+                </li>
+                <li className="flex justify-center gap-x-1">
+                <BiSolidCircle className="border-[#52825F] mt-[6.2px] border rounded-full w-[8px] h-[8px] text-[#34A853]"/>
+                  <span className="text-[#34A853]">Good</span>
+                </li>
+                <li>18/03/2014</li>
+                <li className="flex justify-center items-center gap-x-1 bg-[#4B9A52] mr-2 rounded-[10px] h-[40px] text-white">
+                  Confirm
+                  <AiOutlineDown className="border-slate-50 border rounded-[3px] w-[11px] h-[11px]"/>
+                </li>
+              </ul>
+         
+        </div>
+
+        <div className="flex justify-center gap-x-2 my-[30px]">
+          {/* <div className="flex gap-x-3 mt-1 font-roboto text-[#9054DE]">
+            <h1 className="font-bold text-[16px] uppercase">page</h1>
+            <p className="flex gap-x-1 bg-[#9054DE] px-1 rounded-[4px] h-[20px] text-[14px] text-white">
+              1 <AiOutlineDown className="mt-[5px] w-[12px] h-[12px]"/>
+            </p>
+            <h1 className="font-bold text-[16px] uppercase">of</h1>
+            <p className="font-bold text-[16px]">10</p>
+          </div> */}
+
+              <div className="flex ml-[20px] text-[16px]">
+                <Button className="flex font-bold font-roboto text-[#9054DE] uppercase"><BsChevronDoubleLeft/>First</Button>
+                <Stack spacing={1}>
+                  <Pagination 
+                    // count={getGroups?.meta.totalPages} 
+                    // page = {pageCount}
+                    // onChange={handlePageChange}
+                    defaultPage={1}
+                    boundaryCount={1}
+                    variant="outlined" 
+                    shape="rounded" 
+                    sx={{
+                      '& .MuiPaginationItem-root': {
+                        backgroundColor: '#9054DE',
+                        color: 'white',
+                      },
+                      '& .MuiPaginationItem-root.Mui-selected': {
+                        backgroundColor: '#6F1DCE',
+                        color: 'black'
+                      },
+                      '& .MuiPaginationItem-previousNext': {
+                        backgroundColor: 'transparent',
+                        border: "none", 
+                        color: '#9054DE',
+                        '&:hover': {
+                          backgroundColor: '#f0f0f0', 
+                        },
+                      },
+                      '& .MuiPaginationItem-ellipsis': {
+                        backgroundColor: 'transparent',
+                        color: '#9054DE',  
+                      },
+                  }}
+                  />
+                </Stack>
+                <Button className="flex font-bold font-roboto text-[#9054DE] uppercase">Last <BsChevronDoubleRight/> </Button>
+              </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/admin/Profile.tsx
+++ src/components/admin/Profile.tsx
@@ -1,7 +1,232 @@
+import { Cover, Facebook, friSuggestionProfile, Instagram, Twitter } from "@/assets";
+import "./type.css";
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "../ui/button";
+import { BiPencil } from "react-icons/bi";
+import { VscSettingsGear } from "react-icons/vsc";
+import { LiaUserEditSolid } from "react-icons/lia";
+import { useGetMe } from "@/hooks";
 
 const Profile = () => {
+  const {data: getMyProfile} = useGetMe();
+  console.log(getMyProfile)
+
   return (
-    <div>Profile</div>
+    <div className="flex flex-col w-full overflow-auto vertical-scrollbar">
+      <div className="relative mx-auto mt-[4px] rounded-[10px] w-11/12 h-[180px]">
+        <img src={Cover} alt="" className="rounded-[10px] w-full h-full object-cover"/>
+        <BiPencil className="absolute top-2 right-2 w-[24px] h-[24px] text-[24px] cursor-pointer"/>
+
+        <div className="bottom-0 absolute inset-x-0 border-white/20 bg-slate-400 bg-opacity-30 backdrop-blur-md backdrop-filter mx-auto border rounded-[10px] w-4/5 h-[130px] transform translate-y-1/2">
+          <div className="flex mt-[20px]">
+          <img 
+            src={typeof getMyProfile?.profile === 'string' ? getMyProfile.profile : getMyProfile?.profile ? URL.createObjectURL(getMyProfile.profile) : undefined} 
+            alt="" 
+            className="ml-[32px] rounded-[10px] w-[80px] h-[80px]"
+          />
+            <div className="flex flex-col my-auto ml-[17px] font-poppins">
+              <h1 className="text-[21px]">{getMyProfile?.displayName}</h1>
+              <p className="opacity-80 text-[16px]">Founder of TCU</p>
+            </div>
+
+            <div className="flex gap-x-1 my-auto mr-[30px] ml-auto font-poppins">
+              <VscSettingsGear className="mt-[3px] w-[24px] h-[24px]"/>
+              <h1 className="text-[20px]">Setting</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex mb-4 justify-center gap-x-5 mt-[100px]">
+        <div className="flex flex-col border-white/20 bg-slate-400 bg-opacity-30 backdrop-blur-md backdrop-filter px-[30px] border rounded-[20px] w-[480px] h-[448px] font-poppins">
+          <div className="flex justify-between mt-[17px]">
+            <h1 className="text-[24px]">Profile Information</h1>
+            <LiaUserEditSolid className="mt-1 w-[24px] h-[24px]"/>
+          </div>
+          <p className="opacity-65 mt-[12px] text-[16px]">{getMyProfile?.bio} </p>
+          <h2 className="mt-[30px] text-[20px]">Full Name : <span>{getMyProfile?.displayName}</span></h2>
+          <h2 className="mt-[30px] text-[20px]">Mobile : <span>(95)2544444444</span></h2>
+          <h2 className="mt-[30px] text-[20px]">Email : <span>{getMyProfile?.email}</span></h2>
+          <h2 className="mt-[30px] text-[20px]">Location : <span>Myanmar</span></h2>
+          <h2 className="flex gap-x-1 mt-[30px] text-[20px]">Social : 
+            <span className="flex items-center gap-x-1">
+              <img src={Facebook} alt="" className="w-[40px] h-[33px]"/>
+              <img src={Instagram} alt="" className="w-[40px] h-[40px]"/>
+              <img src={Twitter} alt="" className="w-[30px] h-[26px]"/>
+            </span>
+          </h2>
+        </div>
+
+        <div className="border-white/20 mb-4 bg-slate-400 bg-opacity-30 backdrop-blur-md backdrop-filter border rounded-[20px] w-[480px]">
+          <div className="mt-[17px] px-[30px] font-poppins text-[24px]">
+            <h1 className="">Conversation</h1>
+          </div>
+
+          <div className="flex flex-col md:max-h-[62vh] overflow-auto scrollbar-hide">
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px]">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+            <div className="flex bg-white mx-[10px] mt-[10px] rounded-[10px] h-[80px] ">
+              <Avatar className="top-[5px] ml-[12px]">
+                <AvatarImage src={friSuggestionProfile} />
+              </Avatar> 
+              <div className="mx-[16px] my-auto">
+                <h1 className="font-medium text-[20px]">Person 1</h1>
+                <p className="text-[16px]">Hey You!</p>
+              </div>
+
+              <div className="my-auto mr-2 ml-auto">
+                <Button className="bg-[#B895E7] bg-opacity-40 rounded-[4px]">Reply</Button>
+              </div>
+            </div>
+
+
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
